469f45657f1db7c6be01da92342383df86d72d95
Maxime Biais <maxime.biais@gmail.com>
2011-12-20 11:15:18 +0100

    add connection to mongodb in run script

--- run-if-not.sh
+++ run-if-not.sh
@@ -4,7 +4,8 @@ pid=$(ps x|grep "python"|grep "httpd.py"| grep -v grep |sed "s/^ *//"|cut -d " "
 
 if [ -z "$pid" ]; then
     echo "Start mongodb REST api"
-    python /home/bbsrv/tipcode/sleepy.mongoose/httpd.py
+    python /home/bbsrv/tipcode/sleepy.mongoose/httpd.py 2> /dev/null > /dev/null &
+    curl --data server=localhost:27017 'http://localhost:27080/_connect'
 fi
 
 
