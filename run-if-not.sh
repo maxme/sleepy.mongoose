f7451f7e70f8912ae4592b6a75d42c0689334245
Maxime Biais <maxime.biais@gmail.com>
2011-12-20 11:12:51 +0100

    run script

---
+++ run-if-not.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pid=$(ps x|grep "python"|grep "httpd.py"| grep -v grep |sed "s/^ *//"|cut -d " " -f 1)
+
+if [ -z "$pid" ]; then
+    echo "Start mongodb REST api"
+    python /home/bbsrv/ipcode/sleepy.mongoose/httpd.py
+fi
