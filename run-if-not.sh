a1f8112826c232a848abfccdf9cc9581298e4e13
devs server <devs@mdrive>
2011-12-20 16:44:59 +0000

    remove auto connect

--- run-if-not.sh
+++ run-if-not.sh
@@ -6,7 +6,7 @@ if [ -z "$pid" ]; then
     echo "Start mongodb REST api"
     python /home/bbsrv/tipcode/sleepy.mongoose/httpd.py >> /home/bbsrv/tipcode/sleepy.mongoose/http.log 2>&1  &
     # wait for the server to start
-    sleep 5
+    # sleep 5
     # connect to mongodb
-    curl --data server=mongodb://localhost:27017 'http://localhost:27080/_connect'
+    # curl --data server=mongodb://localhost:27017 'http://localhost:27080/_connect'
 fi
