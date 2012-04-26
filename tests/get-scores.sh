b245c22ea155ad7b1467f443ab4e1cd22c6965ad
Maxime Biais <maxime.biais@gmail.com>
2012-04-26 10:19:46 +0200

    score test

--- tests/get-scores.sh
+++ tests/get-scores.sh
@@ -1,16 +1,24 @@
+HOST=gameboy.iopixel.com/mongodb
+#HOST=mdrive.iopixel.com/mongodb
+#HOST=localhost:27080
 
-curl --data "id=idquinexistepas&leaderboard_id=12&salt=103950784&page_size=10" http://localhost:27080/tipscore/_get_scores
-echo
-curl --data "id=pouet5&leaderboard_id=12&salt=103950784&page_size=100" http://localhost:27080/tipscore/_get_scores
-echo
-curl --data "id=pouet5&leaderboard_id=12&salt=103950784&page_size=1" http://localhost:27080/tipscore/_get_scores
-echo
-curl --data "id=pouet5&leaderboard_id=12&salt=103950784&page_size=2" http://localhost:27080/tipscore/_get_scores
-echo
-curl --data "id=pouet5&leaderboard_id=12&salt=103950784&page_size=3" http://localhost:27080/tipscore/_get_scores
-echo
-curl --data "id=pouet5&leaderboard_id=12&salt=103950784&page_size=4" http://localhost:27080/tipscore/_get_scores
+CAREER_SCORE_LBID=874496
+TWOMN_SCORE_LBID=745187
+FIVEMN_SCORE_LBID=896367
+NETWORK_SCORE_LBID=896377
+
+#curl --data "id=idquinexistepas&leaderboard_id=$CAREER_SCORE_LBID&salt=103950784&page_size=10" http://$HOST/tipscore/_get_scores
+#echo
+#curl --data "id=pouet5&leaderboard_id=$CAREER_SCORE_LBID&salt=103950784&page_size=100" http://$HOST/tipscore/_get_scores
+#echo
+#curl --data "id=pouet5&leaderboard_id=$CAREER_SCORE_LBID&salt=103950784&page_size=1" http://$HOST/tipscore/_get_scores
+#echo
+#curl --data "id=pouet5&leaderboard_id=$CAREER_SCORE_LBID&salt=103950784&page_size=2" http://$HOST/tipscore/_get_scores
+#echo
+#curl --data "id=pouet5&leaderboard_id=$CAREER_SCORE_LBID&salt=103950784&page_size=3" http://$HOST/tipscore/_get_scores
+#echo
+#curl --data "id=pouet5&leaderboard_id=$CAREER_SCORE_LBID&salt=103950784&page_size=4" http://$HOST/tipscore/_get_scores
 
 echo
 echo "10 first scores"
-curl --data "leaderboard_id=12&page_size=10&start=0" http://localhost:27080/tipscore/_get_score_page
+curl --data "leaderboard_id=$CAREER_SCORE_LBID&page_size=10&start=0" http://$HOST/tipscore/_get_score_page
